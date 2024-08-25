9cb60a4157e7cddb83e39a726c86b5761fb3b625
ahmedeid2260 <a.email2260@gmail.com>
2024-08-25 18:07:57 +0300

    asyncHandler

--- modules/note/note.controllers.js
+++ modules/note/note.controllers.js
@@ -1,16 +1,17 @@
 import Note from './../../database/model/note.model.js';
+import asyncHandler from "express-async-handler"
 
-export const create = async (req, res, next) => {
+export const create = asyncHandler(async (req, res, next) => {
     const { title, description } = req.body
     !title && !description && res.status(400).json({ message: 'All fields are required' })
 
     const note = new Note({ title, description })
     await note.save()
     res.status(201).json({ message: 'Note created successfully', note })
-}
+})
 
 
-export const update = async (req, res, next) => {
+export const update = asyncHandler(async (req, res, next) => {
     const { title, description } = req.body
     !title && !description && res.status(400).json({ message: 'All fields are required' })
 
@@ -23,19 +24,19 @@ export const update = async (req, res, next) => {
     await note.save()
 
     res.status(200).json({ message: 'Note updated successfully', note })
-}
+})
 
-export const deleteNote = async (req, res, next) => {
+export const deleteNote = asyncHandler(async (req, res, next) => {
     const note = await Note.findOne({ _id: req.params.id })
     !note && res.status(400).json({ message: 'Note not found' })
 
     await Note.deleteOne({ _id: req.params.id })
 
     res.status(200).json({ message: 'Note deleted successfully' })
-}
+})
 
-export const getAll = async (req, res, next) => {
+export const getAll = asyncHandler(async (req, res, next) => {
     const notes = await Note.find()
 
     res.status(200).json({ 'message': 'All notes', notes })
-}
+})
